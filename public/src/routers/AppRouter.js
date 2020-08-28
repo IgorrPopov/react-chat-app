1004d50e4f8d05f9daf6a89f18affcb06f11cad2
IgorrPopov <ginger_tulips@protonmail.com>
2020-08-28 09:31:13 -0700

    final commit

--- public/src/routers/AppRouter.js
+++ public/src/routers/AppRouter.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LoginPage from '../components/LoginPage';
 import Loader from '../components/Loader';
+import NotFoundPage from '../components/NotFoundPage';
 
 const ChatPage = React.lazy(() => import('../components/ChatPage'));
-const NotFoundPage = React.lazy(() => import('../components/NotFoundPage'));
 
 const AppRouter = () => (
   <BrowserRouter>
