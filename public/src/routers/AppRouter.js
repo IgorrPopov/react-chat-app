4f264fa02058e3ff462b278f8400e893c5797ef0
IgorrPopov <ginger_tulips@protonmail.com>
2020-08-27 18:06:19 -0700

    fix Loader in AppRouter.js

--- public/src/routers/AppRouter.js
+++ public/src/routers/AppRouter.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LoginPage from '../components/LoginPage';
+import Loader from '../components/Loader';
 
 const ChatPage = React.lazy(() => import('../components/ChatPage'));
 const NotFoundPage = React.lazy(() => import('../components/NotFoundPage'));
-const Loader = React.lazy(() => import('../components/Loader'));
 
 const AppRouter = () => (
   <BrowserRouter>
